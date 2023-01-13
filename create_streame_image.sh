a42190f10830e8a9afca930d26812ad8df9c6dc7
anas benmalek <71811170+anasbenmalek@users.noreply.github.com>
2023-01-13 18:33:39 +0100

    Update create_streame_image.sh

--- create_streame_image.sh
+++ create_streame_image.sh
@@ -2,7 +2,7 @@ mkdir streame_dir
 echo "FROM node:12.4-alpine" > streame_dir/Dockerfile
 echo "RUN mkdir /streame_app" >> streame_dir/Dockerfile
 echo "WORKDIR /streame_app" >> streame_dir/Dockerfile
-echo "COPY package.json package.json" >> streame_dir/Dockerfile
+echo "COPY ./StreaMe-App/package.json package.json" >> streame_dir/Dockerfile
 echo "RUN npm install && mv node_modules /node_modules" >> streame_dir/Dockerfile
 echo "COPY . ." >> streame_dir/Dockerfile
 echo "CMD node app.js" >> streame_dir/Dockerfile
